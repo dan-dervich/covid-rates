ea70f10932f06fecea48562440071c4cc388becf
eldervo <messirw@hotmail.com>
2022-02-08 11:30:03 -0300

    initial commit

---
+++ components/selectHandler.tsx
@@ -0,0 +1,6 @@
+
+function selectHandler(e:any){
+    console.log(e.target.value)
+}
+
+export default selectHandler
--- pages/c-d-c.tsx
+++ pages/c-d-c.tsx
@@ -1,13 +1,37 @@
-import { Text } from "@nextui-org/react";
+import { Spacer, Text } from "@nextui-org/react";
 import {Component} from "react";
 import Nav from "../components/nav";
+import selectHandler from '../components/selectHandler'
 
 class Home extends Component{
+    static async getInitialProps(){
+        const res:any = await fetch('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.json')
+        const data:any = await res.json()
+        return data
+    }
+    constructor(props:any) {
+        super(props);
+        this.state = {country: 0};  
+    }
     render(){
+        let countries: string[] = []
+        for(let i in this.props){
+            countries.push(i)
+        }
+        let i:number = 0;        
         return(
             <>
             <Nav />
+            <Spacer y={3}/>
             <Text h1>Covid Data By Country</Text>
+            <select onInput={selectHandler}>
+                {countries.map((country)=>{
+                    i++
+                    return(
+                        <option value={country} key={i}>{country}</option>
+                    )
+                })}
+            </select>
             </>
         )
     }
