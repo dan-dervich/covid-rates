480e41384862192af36fa18fe3d733aba15b60ac
eldervo <messirw@hotmail.com>
2022-02-10 21:15:56 -0300

    some frontend changes

--- README.md
+++
@@ -1,6 +0,0 @@
-- TODOS:
-
-[ ] ALL USA covid data
-[ ] By state covid data
-[ ] all states covid data
-[ ] specific state and date
--- components/main.tsx
+++ components/main.tsx
@@ -1,4 +1,5 @@
 import { Button, Grid, Spacer, Text } from "@nextui-org/react";
+import Image from "next/image";
 import Link from "next/link";
 
 function Main(){
@@ -9,7 +10,7 @@ function Main(){
 return(
     <main>
     <Grid.Container justify="center" direction="row" alignItems="center" style={mainStyles}>
-        <Grid.Container>
+        <Grid xs={5} justify="center" alignItems="center" direction="column">
         <Text h1>Covid Data</Text>
         <Spacer y={2}/>
         <Link href="all-covid-data">
@@ -17,7 +18,10 @@ return(
         <Button style={{fontSize: '1.25em'}} color="gradient">See Global Covid Data</Button>
             </a>
         </Link>
-        </Grid.Container>
+        </Grid>
+        <Grid xs={5} justify="center" alignItems="center">
+        <img src="/something.svg" width={600} alt="SVG" />
+        </Grid>
     </Grid.Container>
     </main>
 )
